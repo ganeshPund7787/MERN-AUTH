3c58fedd0ef762b77fd6f686baa05147fe639c52
Ganesh Pund <ganupund7787@gmail.com>
2024-05-13 11:12:37 -0700

    Database setup

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,10 @@
 import express from 'express'
 import { config } from 'dotenv'
+import { mongoConnection } from './data/data.js'
+
 config({ path: './config/.env' })
+
+mongoConnection()
 const app = express()
 app.listen(process.env.PORT, () => {
     console.log(`server is working on port ${process.env.PORT}`)
